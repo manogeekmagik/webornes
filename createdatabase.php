d780e2969694108fdbfd118618f2f54cdf474e13
Alexandre <jelen18@live.fr>
2019-11-27 15:59:29 +0100

    modif du 27

--- createdatabase.php
+++ createdatabase.php
@@ -7,9 +7,9 @@ $dbname = "webornes";
 // Create database +connexion
 try {
     $connexion = new PDO("mysql:host=$servername;$dbname", $username,$password); 
-    $connexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    $connexion ->execute("CREATE DATABASE IF NOT EXISTS webornes CHARACTER SET utf8mb4 COLLATE utf8mb4_bin");
+    $connexion->exec("CREATE DATABASE IF NOT EXISTS webornes CHARACTER SET utf8mb4 COLLATE utf8mb4_bin");
     
 
     echo 'Base de données créer'; 
